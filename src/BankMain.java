3011ae28385fa48256b6df631fa8e35c03a85467
Jorge <ecnavda@gmail.com>
2017-08-29 23:36:53 -0400

    Removed () from BankMain class declaration and added newline characters to seperate output of tests better.

--- src/BankMain.java
+++ src/BankMain.java
@@ -1,4 +1,4 @@
-public class BankMain() {
+public class BankMain {
 
 	public static void main(String[] args) {
 		System.out.println("Testing default constructor.");
@@ -6,27 +6,27 @@ public class BankMain() {
 		System.out.println(testAccount);
 
 		System.out.println("Testing overloaded parameter constructor.");
-		BankAccount testAccount = new BankAccount(1000.0, .06);
+		testAccount = new BankAccount(1000.0, .06);
 		System.out.println(testAccount);
 
-		System.out.println("Testing SetInterestRate method.");
-		BankAccount testAccount = new BankAccount();
+		System.out.println("\nTesting SetInterestRate method.");
+		testAccount = new BankAccount();
 		System.out.println("testAccount.SetInterestRate(.06)");
 		testAccount.SetInterestRate(.06);
 		System.out.println("Testing GetInterestRate method.");
 		System.out.println("The interest rate is: " + testAccount.GetInterestRate());
 
-		System.out.println("Testing MakeDeposit method.");
+		System.out.println("\nTesting MakeDeposit method.");
 		testAccount.MakeDeposit(1000.05);
-		System.out.println("testAccount.MakeDeposit(1000.05));
+		System.out.println("testAccount.MakeDeposit(1000.05)");
 		System.out.println("Testing GetBalance method.");
 		System.out.println("The balance is: " + testAccount.GetBalance());
 		
-		System.out.println("Testing MakeWithdrawl method.");
+		System.out.println("\nTesting MakeWithdrawl method.");
 		testAccount.MakeWithdrawl(500.0);
 		System.out.println("The balance is: " + testAccount.GetBalance());
 		
-		System.out.println("Testing the Compound method with the existing balance.");
+		System.out.println("\nTesting the Compound method with the existing balance.");
 		testAccount.Compound();
 		System.out.println("testAccount.Compound()");
 		System.out.println("The balance after compound interest is: " + testAccount.GetBalance());
